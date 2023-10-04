407ea5a5bf526372d314d0b503f2c10f80ab2ccd
Rounak-Ghosh <rounakghosh189@gmail.com>
2023-10-04 23:46:03 +0530

    Website under construction

---
+++ src/pages/Frame.js
@@ -0,0 +1,34 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <div className="div">
+      <div className="under-construction-parent">
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+      </div>
+      <div className="under-construction-group">
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+        <div className="under-construction">Under Construction</div>
+        <div className="under-construction">×</div>
+      </div>
+      <div className="website-under-construction">
+        Website Under Construction
+      </div>
+      <div className="lorem-ipsum-dolor">{`BTP Clone Site Under Construction. A simple process automation is been tried to build by two psycho graduates aka associate consultants💀 `} <br></br><br></br> {`Generated using Locofy.ai plugin. `}</div>
+    </div>
+  );
+};
+
+export default Frame;
